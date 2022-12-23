9ffb62915bce21fe43ce9ffc9fabb10e65514879
Gaizka79 <agaizka@hotmail.com>
2022-12-23 01:45:42 +0100

    Front aurrera

--- client/src/components/Main/Home/Home.jsx
+++ client/src/components/Main/Home/Home.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import User from '../User/User';
 
 function Home () {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.get("/users")
+      .then((res)=> {
+        console.log(res.data)
+        //setData(res.data[0].nombre);
+        setData(res.data)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  },[]);
+
+  const paintUsers = () => {
+    return data.map(
+      (user,i) => (
+      <User value={user} key={i}/>))
+  };
+
   return (
     <div className="home">
-      <strong>Home</strong>
+      {paintUsers()}
+      {/* {!data ? <p>"Loading..."</p> : data.map((temp,i) => <p value={temp.nombre} key={i}/>)} */}
     </div>
   )
 }
